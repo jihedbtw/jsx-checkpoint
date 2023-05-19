bc8db64aa99e1e6f30d6ad4c74e3ece8955dedc7
jihedbtw <116655749+jihedbtw@users.noreply.github.com>
2023-05-19 20:26:05 +0100

    Add files via upload

---
+++ src/component/profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProfilePhoto() {
+return (
+    <div>
+    <img src="/doctor strange.jpg" alt="Profile Photo" />
+    </div>
+);
+}
+
+export default ProfilePhoto;
